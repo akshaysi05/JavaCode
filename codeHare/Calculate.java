e3d8292729512d7c6fdc79ce5e5d6efaa51c2c7f
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-17 14:51:14 +0530

    MethodsCallOntherClass

---
+++ codeHare/Calculate.java
@@ -0,0 +1,17 @@
+ public class Calculate {
+
+    public static void main(String[] args) {
+        int num1=4;
+        int num2=5;
+        DemoCall d= new DemoCall();
+        int res=  d.add(num1,num2);
+       System.out.println(res);
+
+    }
+    
+}
+ 
+ 
+ 
+
+
---
+++ codeHare/DemoCall.java
@@ -0,0 +1,9 @@
+public class DemoCall {
+    
+    public int add(int n1,int n2)
+    {
+      int result=  n1 + n2;
+      return result;
+        
+    }
+}
