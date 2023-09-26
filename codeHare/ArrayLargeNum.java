ebc5530bd753ae44b85fd3550339f06ae887b6d6
akshaysi05 <akshaysingh8804@gmail.com>
2023-09-26 17:26:13 +0530

    Java Code

---
+++ codeHare/ArrayLargeNum.java
@@ -0,0 +1,13 @@
+public class ArrayLargeNum {
+    public static void main(String[] args) {
+        int num[]={5,8,9,3,4};
+        int num1=0;
+
+        for(int i=1;i>num.length;i++){
+            if (num[i]>num1) {
+                System.out.println(num[i]);
+                
+            }
+        }
+    }
+}
