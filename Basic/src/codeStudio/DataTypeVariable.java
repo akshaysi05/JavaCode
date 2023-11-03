b3f44908e3827a3f351f72b69b4c87a3d3b4001d
akshaysi05 <akshaysingh8804@gmail.com>
2023-11-03 18:07:31 +0530

    learn DataTypeVariable

---
+++ Basic/src/codeStudio/DataTypeVariable.java
@@ -0,0 +1,19 @@
+package codeStudio;
+
+public class DataTypeVariable {
+
+	public static void main(String[] args) {
+		
+		//Declaration:
+		int a;
+		//initialization:
+		a=10;
+		
+		//togather:
+		int b=20;
+		System.out.println("a:"+a);
+
+		System.out.print("b:"+b);
+	}
+
+}
